319368f0d00e9954e399eb2a486198c2f2aa689b
Jullian York G. Santos <86148196+qjygsantos@users.noreply.github.com>
2024-05-04 19:46:48 +0800

    Update create_csv.py

--- util_scripts/create_csv.py
+++ util_scripts/create_csv.py
@@ -1,36 +1,36 @@
-# Script to create CSV data file from Pascal VOC annotation files
-# Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
-
 import os
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-def xml_to_csv(path):
+def xml_to_csv(path)
     xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
+    for xml_file in glob.glob(path + '.xml')
         tree = ET.parse(xml_file)
         root = tree.getroot()
-        for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
+        for member in root.findall('object')
+            filename = root.find('filename').text
+            size = root.find('size')
+            width = int(size.find('width').text)
+            height = int(size.find('height').text)
+            name = member.find('name').text
+            
+            xmin = int(member.find('bndboxxmin').text) if member.find('bndboxxmin') is not None else None
+            ymin = int(member.find('bndboxymin').text) if member.find('bndboxymin') is not None else None
+            xmax = int(member.find('bndboxxmax').text) if member.find('bndboxxmax') is not None else None
+            ymax = int(member.find('bndboxymax').text) if member.find('bndboxymax') is not None else None
+            
+            xml_list.append((filename, width, height, name, xmin, ymin, xmax, ymax))
+
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
-def main():
-    for folder in ['train','validation']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+def main()
+    for folder in ['train','validation']
+        image_path = os.path.join(os.getcwd(), ('images' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv(('images' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
 
 main()
